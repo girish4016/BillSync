d45c0cc1a4833f5656a53371a6de38084dfb1b34
Vikas Kumar Sharma <svikaskumar23@gmail.com>
2024-05-22 00:20:17 +0530

    added user settings page

--- Frontend/app/src/App.tsx
+++ Frontend/app/src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useRoutes } from "react-router-dom";
 import { HomeRoute } from "./common/constants/route-name";
 import { HomeRoutes } from "./common/routes/HomeRoutes";
@@ -14,6 +15,16 @@ function App() {
   ]);
 
   return <div>{routes}</div>;
+=======
+import Settings from "./components/Settings"
+
+function App() {
+  return (
+    <div>
+      <Settings/>
+    </div>
+  );
+>>>>>>> 78074f7 (added user settings page)
 }
 
 export default App;
