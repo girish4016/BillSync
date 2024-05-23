5263f32ac85f4f330e2d80b634c073c66bc9f476
girish basandani <gbasandani@betatestsolutions.com>
2024-05-23 13:48:48 +0530

    created navbar ui

--- Frontend/app/src/App.tsx
+++ Frontend/app/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { Button } from "./common/shadcn/ui/button";
 
+import { NavBar } from "./common/components/Navbar/NavBar";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <NavBar isHomePage={true} />
       <Button onClick={() => setCount((prev) => prev + 1)}>
         tap to increase : {count}
       </Button>
