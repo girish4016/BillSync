738742ac02ba4caa4237998dcf89b74bf1ab3940
girish basandani <gbasandani@betatestsolutions.com>
2024-06-12 01:52:22 +0530

    added group details component and routing for it

---
+++ Frontend/app/src/components/Groups/components/GroupDetails.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from "@/common/components/Navbar/NavBar";
+
+export const GroupDetails = () => {
+  return (
+    <div className="contain-paint">
+      <NavBar />
+    </div>
+  );
+};
