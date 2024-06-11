7af015bd6bd7c7e67bda6d3fd19ed3d2cd96e682
girish basandani <gbasandani@betatestsolutions.com>
2024-06-12 01:52:22 +0530

    added group details component and routing for it

--- Frontend/app/src/common/constants/route-name.ts
+++ Frontend/app/src/common/constants/route-name.ts
@@ -1,6 +1,7 @@
 export const HomeRoute = "/";
 
 export const GroupsRoute = "/groups";
+export const GroupDetailsRoute = "/groups/group-details/:groupId";
 
 export const FriendsRoute = "/friends";
 
